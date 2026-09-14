9288cefa099dd5454db99c33643ec0aa5681beb5
arzenikos <arzenikos@gmail.com>
2026-09-14 15:23:23 +1200

    Rename content subdirectories

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,50 +11,50 @@ export default defineConfig({
         enabled: false
     },
     fonts: [
-        {
-            provider: fontProviders.local(),
-            name: 'Gotham',
-            cssVariable: '--font-gotham',
-            fallbacks: ['sans-serif'],
-            options: {
-                variants: [
-                    {
-                        src: ['/assets/fonts/GothamSSm/gothamcondssm_black.otf'],
-                        weight: 400,
-                        style: 'normal',
-                        display: 'swap',
-                    },
-                    {
-                        src: ['/assets/fonts/GothamSSm/gothamxnarrssm_bold.otf'],
-                        weight: 700,
-                        style: 'bold',
-                        display: 'swap',
-                    },
-                ],
-            },
-        },
-        {
-            provider: fontProviders.local(),
-            name: 'Atkinsons',
-            cssVariable: '--font-atkinsons',
-            fallbacks: ['sans-serif'],
-            options: {
-                variants: [
-                    {
-                        src: ['/assets/fonts/atkinsons/atkinson-bold.woff'],
-                        weight: 500,
-                        style: 'bold',
-                        display: 'swap',
-                    },
-                    {
-                        src: ['/assets/fonts/atkinsons/atkinson-regular.woff'],
-                        weight: 200,
-                        style: 'normal',
-                        display: 'swap',
-                    }
-                ],
-            },
-        }
+        // {
+        //     provider: fontProviders.local(),
+        //     name: 'Gotham',
+        //     cssVariable: '--font-gotham',
+        //     fallbacks: ['sans-serif'],
+        //     options: {
+        //         variants: [
+        //             {
+        //                 src: ['/assets/fonts/GothamSSm/gothamcondssm_black.otf'],
+        //                 weight: 400,
+        //                 style: 'normal',
+        //                 display: 'swap',
+        //             },
+        //             {
+        //                 src: ['/assets/fonts/GothamSSm/gothamxnarrssm_bold.otf'],
+        //                 weight: 700,
+        //                 style: 'bold',
+        //                 display: 'swap',
+        //             },
+        //         ],
+        //     },
+        // },
+        // {
+        //     provider: fontProviders.local(),
+        //     name: 'Atkinsons',
+        //     cssVariable: '--font-atkinsons',
+        //     fallbacks: ['sans-serif'],
+        //     options: {
+        //         variants: [
+        //             {
+        //                 src: ['/assets/fonts/atkinsons/atkinson-bold.woff'],
+        //                 weight: 500,
+        //                 style: 'bold',
+        //                 display: 'swap',
+        //             },
+        //             {
+        //                 src: ['/assets/fonts/atkinsons/atkinson-regular.woff'],
+        //                 weight: 200,
+        //                 style: 'normal',
+        //                 display: 'swap',
+        //             }
+        //         ],
+        //     },
+        // }
     ],
     integrations: [
         react(),
